84f4b93685f45711031a8a89fafe64da4fcc5e87
Douglas Freitas <douglas.freitas@gou.cooking>
2018-08-22 08:15:48 -0300

    logout and login. block SSR on private area

--- src/shared/Login.js
+++ src/shared/Login.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 
 class Login extends Component {
   constructor(props) {
     super(props)
 
+    let isAuth = false
+    if (__isBrowser__) {
+      isAuth = window.localStorage.getItem('auth')
+    }
+
     this.state = {
       redirectToReferrer: false,
-      login: window.localStorage.getItem('auth')
+      login: isAuth
     }
+
+    this.login = this.login.bind(this)
+    this.logout = this.logout.bind(this)
   }
 
   login() {
@@ -17,11 +26,19 @@ class Login extends Component {
       login: window.localStorage.getItem('auth')
     }))
   }
+
+  logout() {
+    window.localStorage.setItem('auth', false)
+    this.setState(() => ({
+      redirectToReferrer: false,
+      login: window.localStorage.getItem('auth')
+    }))
+  }
   
   render() {
     const { from } = this.props.location.state || { from: { pathname: '/' } }
     const { redirectToReferrer } = this.state
-
+    console.log('from', from)
     if (redirectToReferrer === true) {
       return <Redirect to={from} />
     }
@@ -30,6 +47,7 @@ class Login extends Component {
       <div>
         <p>You must log in to view the page</p>
         <button onClick={this.login}>Log in</button>
+        <button onClick={this.logout}>Log out</button>
       </div>
     )
   }
--- src/utils/PrivateRoute.js
+++ src/utils/PrivateRoute.js
@@ -4,14 +4,22 @@ import { Route, Redirect } from 'react-router-dom'
 const PrivateRoute = ({ component: Component, ...rest }) => {
   console.log(rest);
 
-  return <Route {...rest} render={(props) => (
-    window.localStorage.getItem('auth') === "true"
-      ? <Component {...props} />
-      : <Redirect to={{
+  return <Route {...rest} render={(props) => {
+    let isAuth = false
+    if(__isBrowser__){
+      isAuth = window.localStorage.getItem('auth') === "true";
+    } else {
+
+    }
+    if (isAuth)
+      return <Component {...props} />
+    else
+      return <Redirect to={{
           pathname: '/login',
           state: { from: props.location }
-        }} />
-  )} />
+      }} />
+    }
+  }/>
 }
 
 export default PrivateRoute
